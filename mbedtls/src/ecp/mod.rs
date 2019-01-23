455b639510c64382ec112099e0846b66fbeb5316
Jack Lloyd <jack.lloyd@fortanix.com>
2019-01-23 15:48:04 -0500

    Make cert and ecp unit tests

--- mbedtls/src/ecp/mod.rs
+++ mbedtls/src/ecp/mod.rs
@@ -236,3 +236,119 @@ impl EcPoint {
         Ok(buf)
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use pk::EcGroupId;
+    use ecp::EcGroup;
+    use ecp::EcPoint;
+    use bignum::Mpi;
+
+    #[test]
+    fn test_ec_group() {
+        let secp256r1 = EcGroup::new(EcGroupId::SecP256R1).unwrap();
+
+        assert_eq!(secp256r1.group_id().unwrap(), EcGroupId::SecP256R1);
+
+        let p = secp256r1.p().unwrap().to_binary().unwrap();
+
+        let p256 = vec![
+            0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ];
+
+        assert_eq!(p, p256);
+
+        assert_eq!(secp256r1.cofactor().unwrap(), 1);
+    }
+
+    #[test]
+    #[cfg(feature = "std")]
+    fn test_ecp_encode() {
+        use std::str::FromStr;
+
+        let mut secp256k1 = EcGroup::new(EcGroupId::SecP256K1).unwrap();
+        let bitlen = secp256k1.p().unwrap().bit_length().unwrap();
+        let g = secp256k1.generator().unwrap();
+        assert_eq!(g.is_zero().unwrap(), false);
+
+        let k = Mpi::new(0xC3FF2).unwrap();
+        let pt = g.mul(&mut secp256k1, &k).unwrap();
+
+        let pt_uncompressed = pt.to_binary(&secp256k1, false).unwrap();
+        assert_eq!(pt_uncompressed.len(), 1 + 2 * (bitlen / 8));
+        let rec_pt = EcPoint::from_binary(&secp256k1, &pt_uncompressed).unwrap();
+        assert_eq!(pt.eq(&rec_pt).unwrap(), true);
+
+        let pt_compressed = pt.to_binary(&secp256k1, true).unwrap();
+        assert_eq!(pt_compressed.len(), 1 + bitlen / 8);
+
+        /*
+        Mbedtls supports encoding a point to compressed, but does not
+        support reading it back, so skip trying to do that.
+         */
+
+        let affine_x = pt.x().unwrap();
+        assert_eq!(
+            affine_x,
+            Mpi::from_str("0x1E248FB0AB87942E4B74446F7C9CD151468919B525C108759876F806CA2FFC87")
+                .unwrap()
+        );
+        let affine_y = pt.y().unwrap();
+        assert_eq!(
+            affine_y,
+            Mpi::from_str("0x821F40015051C2E37E85A97D96B83A9948FB108E06C98F5AD2CF275C8A9B004B")
+                .unwrap()
+        );
+        let pt_from_components = EcPoint::from_components(&affine_x, &affine_y).unwrap();
+        assert!(pt.eq(&pt_from_components).unwrap());
+    }
+
+    #[test]
+    fn test_ecp_mul() {
+        let mut secp256r1 = EcGroup::new(EcGroupId::SecP256R1).unwrap();
+
+        let g = secp256r1.generator().unwrap();
+        assert_eq!(g.is_zero().unwrap(), false);
+
+        let k = Mpi::new(380689).unwrap();
+        let half_k = Mpi::new(617).unwrap();
+
+        /*
+        Basic sanity check - multiplying twice by k is same as multiply by k**2
+         */
+        let pt1 = g.mul(&mut secp256r1, &k).unwrap();
+        assert_eq!(pt1.is_zero().unwrap(), false);
+
+        let pt2 = g.mul(&mut secp256r1, &half_k).unwrap();
+        assert_eq!(pt2.is_zero().unwrap(), false);
+        assert_eq!(pt1.eq(&pt2).unwrap(), false);
+
+        let pt3 = pt2.mul(&mut secp256r1, &half_k).unwrap();
+        assert_eq!(pt1.eq(&pt3).unwrap(), true);
+        assert_eq!(pt3.eq(&pt1).unwrap(), true);
+
+        assert_eq!(secp256r1.contains_point(&pt3).unwrap(), true);
+        let secp256k1 = EcGroup::new(EcGroupId::SecP256K1).unwrap();
+        assert_eq!(secp256k1.contains_point(&pt3).unwrap(), false);
+    }
+
+    #[test]
+    fn test_ecp_mul_add() {
+        let mut secp256r1 = EcGroup::new(EcGroupId::SecP256R1).unwrap();
+
+        let g = secp256r1.generator().unwrap();
+
+        let k1 = Mpi::new(1212238156).unwrap();
+        let k2 = Mpi::new(1163020627).unwrap();
+
+        // Test that k1*g + k2*g == k2*g + k1*g
+        let pt1 = EcPoint::muladd(&mut secp256r1, &g, &k2, &g, &k1).unwrap();
+        let pt2 = EcPoint::muladd(&mut secp256r1, &g, &k1, &g, &k2).unwrap();
+        assert_eq!(pt1.eq(&pt2).unwrap(), true);
+
+        // Todo a better test ...
+    }
+}
--- mbedtls/src/x509/certificate.rs
+++ mbedtls/src/x509/certificate.rs
@@ -182,7 +182,7 @@ impl LinkedCertificate {
     }
 
     pub fn digest_type(&self) -> ::hash::Type {
-	::hash::Type::from(self.inner.sig_md)
+        ::hash::Type::from(self.inner.sig_md)
     }
 
     pub fn verify(
@@ -732,5 +732,61 @@ yj9wo3CnzOvVa5Yt1HWfKWYX3RVeSDvOaN2rrKauiVQ/KPSD9gWrySCt7Kqp+Ruk
             .write_pem_string(&mut ::test_support::rand::test_rng())
             .unwrap();
         assert_eq!(output, TEST_PEM);
+    }
+
+    #[test]
+    fn channel_binding_hash() {
+        const TEST_CERT_PEM: &'static str = "-----BEGIN CERTIFICATE-----
+MIIDLDCCAhSgAwIBAgIRALY0SS5pY9Yb/aIHvSAvmOswDQYJKoZIhvcNAQELBQAw
+HzEQMA4GA1UEAxMHVGVzdCBDQTELMAkGA1UEBhMCVVMwHhcNMTkwMTA4MDAxODM1
+WhcNMjkwMTA1MDAxODM1WjAjMRIwEAYDVQQDEwlUZXN0IENlcnQxDTALBgNVBAoT
+BFRlc3Qwgd8wDQYJKoZIhvcNAQEBBQADgc0AMIHJAoHBAKYINzSAKG1/Kn/5dWXq
+cfJgfQkzVn1HPzdb4NNZL+H7woGuzDGrcQ7EPi7r4EuAEE2fCjhSfiYlacoBOxd/
+k9Fp4Iv2ygCY1nj8RY0tFCZcZDVYj5F7uqyJMf7+QSOpnZ4cb3zdj1HkBmq7ac0C
+7tXkubvM6gBS3H3XlhfszcEjvhavaxVVoitdqW8RJ2DHvqGwFUxPgFCuuQudeCI/
+UzBiPMRqu3Pr9Xhcc0ruG5SkCg5isbWWnKNj7X1gTre6WwIDAQABo4GiMIGfMCEG
+A1UdDgQaBBhoOfrVfmVEEhzGvEIZU8yWIGVcV8+sBgIwMgYDVR0RBCswKYERdGVz
+dEBmb3J0YW5peC5jb22CFGV4YW1wbGUuZm9ydGFuaXguY29tMAwGA1UdEwEB/wQC
+MAAwIwYDVR0jBBwwGoAYeQdrzI2gB35BFvhLjkycXGr37E+gANmHMBMGA1UdJQQM
+MAoGCCsGAQUFBwMCMA0GCSqGSIb3DQEBCwUAA4IBAQBKSyY45jagwBIbAzTgSzQq
+wXsXixowANXchMBhKUFRnjrJnacoI4CeZD+dHA/3yic0xjl0IVh59ihlMtQ7DaYI
+b7ISqWyPVz3kIwyas64J1iFxnS41s+kZY9XnY6Jz8OJda7xfzQzXrOaIgh3xck+z
+lWyWBGzVgSbzripmaAzMyKrsvmgPpfx5aE7zP2QVOzGXE/QuoXqj/bmblNlUZu11
+5XJ4nSxziKSdNaZZBCn+m2lZiW6GWK7idvNHT/MVBR5mM74jbSrPVSFk6mk2Ei+d
+cYp0bH/RcPTC0Z+ZaqSWMtfxRrk63MJQF9EXpDCdvQRcTMD9D85DJrMKn8aumq0M
+-----END CERTIFICATE-----\0";
+
+        let cert = Certificate::from_pem(&TEST_CERT_PEM.as_bytes()).unwrap();
+
+        assert_eq!(cert.issuer().unwrap(), "CN=Test CA, C=US");
+        assert_eq!(cert.subject().unwrap(), "CN=Test Cert, O=Test");
+        assert_eq!(
+            cert.serial().unwrap(),
+            "B6:34:49:2E:69:63:D6:1B:FD:A2:07:BD:20:2F:98:EB"
+        );
+        assert_eq!(cert.digest_type(), ::hash::Type::Sha256);
+
+        let pk = cert.public_key();
+
+        assert_eq!(pk.pk_type(), ::pk::Type::Rsa);
+        assert_eq!(pk.rsa_public_exponent().unwrap(), 0x10001);
+
+        let channel_binding_hash = match cert.digest_type() {
+            ::hash::Type::Md5 | ::hash::Type::Sha1 => ::hash::Type::Sha256,
+            digest => digest,
+        };
+
+        let mut digest = [0u8; 64];
+        let digest_len =
+            ::hash::Md::hash(channel_binding_hash, cert.as_der(), &mut digest).unwrap();
+
+        assert_eq!(
+            digest[0..digest_len],
+            [
+                0xcc, 0x61, 0xd9, 0x07, 0xc2, 0xcb, 0x49, 0x58, 0x73, 0xbf, 0xd7, 0x43, 0x21, 0xb2,
+                0xd4, 0x30, 0xc6, 0xfe, 0xa6, 0x6c, 0x28, 0x96, 0x23, 0xc6, 0x28, 0x4c, 0xdd, 0x14,
+                0xda, 0x1d, 0xc4, 0x17
+            ]
+        );
     }
 }
