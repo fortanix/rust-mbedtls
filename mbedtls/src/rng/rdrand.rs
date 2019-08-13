3c0621d0b78cb16d23a691684e3f52995ee38130
Jack Lloyd <jack.lloyd@fortanix.com>
2019-08-13 11:32:38 -0400

    Address review comments

--- mbedtls/src/rng/rdrand.rs
+++ mbedtls/src/rng/rdrand.rs
@@ -13,43 +13,61 @@ use mbedtls_sys::types::size_t;
 
 // Intel documentation claims that if hardware is working RDRAND will produce output
 // after at most 10 attempts
-const RDRAND_READ_ATTEMPTS : i32 = 10;
+const RDRAND_READ_ATTEMPTS: usize = 10;
 
 // Intel does not document the number of times RDSEED might consecutively fail, but in
 // example code uses 75 as the upper bound.
-const RDSEED_READ_ATTEMPTS : i32 = 75;
+const RDSEED_READ_ATTEMPTS: usize = 75;
 
-fn call_cpu_rng<T: Sized + Copy>(attempts: i32, intrin: unsafe fn(&mut T) -> i32) -> Option<T> {
-    let mut attempts = attempts;
-    while attempts > 0 {
-        let mut out : T = unsafe { std::mem::uninitialized() };
+fn call_cpu_rng<T, F>(attempts: usize, intrin: unsafe fn(&mut T) -> i32, cast_to_usize: F) -> Option<usize>
+where
+    T: Sized + Copy + Default,
+    F: FnOnce(T) -> usize,
+{
+    for _ in 0..attempts {
+        let mut out = T::default();
         let status = unsafe { intrin(&mut out) };
         if status == 1 {
-            return Some(out);
+            return Some(cast_to_usize(out));
         }
-        attempts -= 1;
     }
     None
 }
 
 #[cfg(target_arch = "x86_64")]
-fn rdrand() -> Option<u64> {
-    call_cpu_rng(RDRAND_READ_ATTEMPTS, core::arch::x86_64::_rdrand64_step)
+fn rdrand() -> Option<usize> {
+    call_cpu_rng(
+        RDRAND_READ_ATTEMPTS,
+        core::arch::x86_64::_rdrand64_step,
+        |x:u64| -> usize { x as usize }
+    )
 }
 
 #[cfg(target_arch = "x86_64")]
-fn rdseed() -> Option<u64> {
-    call_cpu_rng(RDSEED_READ_ATTEMPTS, core::arch::x86_64::_rdseed64_step)
+fn rdseed() -> Option<usize> {
+    call_cpu_rng(
+        RDSEED_READ_ATTEMPTS,
+        core::arch::x86_64::_rdseed64_step,
+        |x:u64| -> usize { x as usize }
+    )
 }
 
 #[cfg(target_arch = "x86")]
-fn rdrand() -> Option<u32> {
-    call_cpu_rng(RDRAND_READ_ATTEMPTS, core::arch::x86::_rdrand32_step)
+fn rdrand() -> Option<usize> {
+    call_cpu_rng(
+        RDRAND_READ_ATTEMPTS,
+        core::arch::x86::_rdrand32_step,
+        |x:u32| -> usize { x as usize }
+    )
 }
 
 #[cfg(target_arch = "x86")]
-fn rdseed() -> Option<u32> {
-    call_cpu_rng(RDSEED_READ_ATTEMPTS, core::arch::x86::_rdseed32_step)
+fn rdseed() -> Option<usize> {
+    call_cpu_rng(
+        RDSEED_READ_ATTEMPTS,
+        core::arch::x86::_rdseed32_step,
+        |x:u32| -> usize { x as usize }
+    )
 }
 
 use super::{EntropyCallback, RngCallback};
@@ -60,16 +78,14 @@ impl EntropyCallback for Entropy {
     unsafe extern "C" fn call(_: *mut c_void, data: *mut c_uchar, len: size_t) -> c_int {
         let outbuf = from_raw_parts_mut(data, len);
 
-        let stepsize = if cfg!(target_arch = "x86_64") { 8 } else { 4 };
+        let stepsize = core::mem::size_of::<usize>();
 
         for chunk in outbuf.chunks_mut(stepsize) {
             if let Some(val) = rdseed() {
-                let mut buf = [0u8; 8];
-                ::core::ptr::copy_nonoverlapping(&val as *const _ as *const u8, buf.as_mut_ptr(), stepsize);
+                let buf = val.to_ne_bytes();
                 let ptr = &buf[..chunk.len()];
                 chunk.copy_from_slice(ptr);
-            }
-            else {
+            } else {
                 return ::mbedtls_sys::ERR_ENTROPY_SOURCE_FAILED;
             }
         }
@@ -87,16 +103,14 @@ impl RngCallback for Nrbg {
     unsafe extern "C" fn call(_: *mut c_void, data: *mut c_uchar, len: size_t) -> c_int {
         let outbuf = from_raw_parts_mut(data, len);
 
-        let stepsize = if cfg!(target_arch = "x86_64") { 8 } else { 4 };
+        let stepsize = core::mem::size_of::<usize>();
 
         for chunk in outbuf.chunks_mut(stepsize) {
             if let Some(val) = rdrand() {
-                let mut buf = [0u8; 8];
-                ::core::ptr::copy_nonoverlapping(&val as *const _ as *const u8, buf.as_mut_ptr(), stepsize);
+                let buf = val.to_ne_bytes();
                 let ptr = &buf[..chunk.len()];
                 chunk.copy_from_slice(ptr);
-            }
-            else {
+            } else {
                 return ::mbedtls_sys::ERR_ENTROPY_SOURCE_FAILED;
             }
         }
