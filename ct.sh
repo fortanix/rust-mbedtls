966b487db315c677ede887eb959a20b8fd493bd4
Jack Lloyd <jack.lloyd@fortanix.com>
2019-10-08 17:32:36 -0400

    Split up Travis into 4 jobs
    This improves parallelism (Travis will run 5 jobs at once) and avoids
    redundancy (the core_io + nightly builds are being run twice, in the
    stable and beta builds).

--- ct.sh
+++ ct.sh
@@ -2,20 +2,28 @@
 set -ex
 cd "$(dirname "$0")"
 
-if [ "${TRAVIS_BRANCH}" = "staging" ];
-then
-    rm -rf ./target
-fi
-
 cd "./mbedtls"
 
-cargo test
-cargo test --features spin_threading
-cargo test --features rust_threading
-cargo test --features zlib
-cargo test --features pkcs12
-cargo test --features pkcs12_rc2
-cargo test --features force_aesni_support
-cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand,time,custom_time,custom_gmtime_r
-cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand
-cargo +nightly test --no-run --target=x86_64-fortanix-unknown-sgx --features=sgx --no-default-features
+if [ $TOOLCHAIN = "stable" ] || [ $TOOLCHAIN = "beta" ]; then
+
+    rustup toolchain add $TOOLCHAIN
+    rustup default $TOOLCHAIN
+    cargo test
+    cargo test --features spin_threading
+    cargo test --features rust_threading
+    cargo test --features zlib
+    cargo test --features pkcs12
+    cargo test --features pkcs12_rc2
+    cargo test --features force_aesni_support
+
+elif [ $TOOLCHAIN = "coreio_nightly" ]; then
+
+    rustup toolchain add $CORE_IO_NIGHTLY
+    cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand,time,custom_time,custom_gmtime_r
+    cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand
+
+elif [ $TOOLCHAIN = "sgx" ]; then
+    rustup toolchain add nightly
+    cargo +nightly test --no-run --target=x86_64-fortanix-unknown-sgx --features=sgx --no-default-features
+
+fi
