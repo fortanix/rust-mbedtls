695a3bb977aacaebcce68cc98a605cd7ed4eb4d7
Jack Lloyd <jack.lloyd@fortanix.com>
2019-10-08 18:49:27 -0400

    Try this instead

--- ct.sh
+++ ct.sh
@@ -4,10 +4,8 @@ cd "$(dirname "$0")"
 
 cd "./mbedtls"
 
-if [ $TOOLCHAIN = "stable" ] || [ $TOOLCHAIN = "beta" ]; then
-
-    rustup toolchain add $TOOLCHAIN
-    rustup default $TOOLCHAIN
+if [ $TRAVIS_RUST_VERSION = "stable" ] || [ $TRAVIS_RUST_VERSION = "beta" ]; then
+    rustup default $TRAVIS_RUST_VERSION
     cargo test
     cargo test --features spin_threading
     cargo test --features rust_threading
@@ -16,15 +14,11 @@ if [ $TOOLCHAIN = "stable" ] || [ $TOOLCHAIN = "beta" ]; then
     cargo test --features pkcs12_rc2
     cargo test --features force_aesni_support
 
-elif [ $TOOLCHAIN = "coreio_nightly" ]; then
-
-    rustup toolchain add $CORE_IO_NIGHTLY
+elif [ $TRAVIS_RUST_VERSION = $CORE_IO_NIGHTLY ]; then
     cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand,time,custom_time,custom_gmtime_r
     cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand
 
-elif [ $TOOLCHAIN = "sgx" ]; then
-
-    rustup toolchain add $SGX_NIGHTLY
+elif [ $TRAVIS_RUST_VERSION = $SGX_NIGHTLY ]; then
     rustup target add --toolchain $SGX_NIGHTLY x86_64-fortanix-unknown-sgx
     cargo +$SGX_NIGHTLY test --no-run --target=x86_64-fortanix-unknown-sgx --features=sgx --no-default-features
 
