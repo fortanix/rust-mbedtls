5cfcb9dc4dac980cdec887dceda9d998a186c5e4
Scott Mabin <scott@mabez.dev>
2019-10-08 14:04:21 +0100

    Add an arm no_std build to ct

--- ct.sh
+++ ct.sh
@@ -19,3 +19,4 @@ cargo test --features force_aesni_support
 cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand,time,custom_time,custom_gmtime_r
 cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand
 cargo +nightly test --no-run --target=x86_64-fortanix-unknown-sgx --features=sgx --no-default-features
+cargo +$CORE_IO_NIGHTLY build --no-default-features --features core_io --target=thumbv7m-none-eabi
