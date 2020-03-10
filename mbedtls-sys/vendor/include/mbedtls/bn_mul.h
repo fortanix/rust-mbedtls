866ccc6a2b3c657ebcd57830496e5bfc8252fc4a
Raoul Strackx <raoul.strackx@fortanix.com>
2020-03-10 16:51:47 +0100

    Adding mpi_force_c_code feature
    (cherry picked from commit 38522c212a441cb0c58b1ae6be6f2f43e3affb99)

--- mbedtls-sys/vendor/include/mbedtls/bn_mul.h
+++ mbedtls-sys/vendor/include/mbedtls/bn_mul.h
@@ -73,7 +73,7 @@
 #endif /* bits in mbedtls_mpi_uint */
 
 /* *INDENT-OFF* */
-#if defined(MBEDTLS_HAVE_ASM)
+#if defined(MBEDTLS_HAVE_ASM) && !defined(MBEDTLS_MPI_FORCE_C_CODE)
 
 #ifndef asm
 #define asm __asm
@@ -952,7 +952,7 @@
 #endif /* SSE2 */
 #endif /* MSVC */
 
-#endif /* MBEDTLS_HAVE_ASM */
+#endif /* MBEDTLS_HAVE_ASM && !MBEDTLS_MPI_FORCE_C_CODE */
 
 #if !defined(MULADDC_CORE)
 #if defined(MBEDTLS_HAVE_UDBL)
