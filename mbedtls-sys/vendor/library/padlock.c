e8481747f2d79a0459ba487435662f1dbdce04b4
Raoul Strackx <raoul.strackx@fortanix.com>
2020-12-16 16:51:52 +0100

    Replace `vendor` with `mbedtls-2.25.0`
    The `mbedtls-2.25.0` library was downloaded from `https://github.com/ARMmbed/mbedtls/archive/v2.25.0.zip`

--- mbedtls-sys/vendor/library/padlock.c
+++ mbedtls-sys/vendor/library/padlock.c
@@ -37,7 +37,6 @@
 
 #if defined(MBEDTLS_HAVE_X86)
 
-#if !defined(MBEDTLS_CUSTOM_HAS_PADLOCK)
 /*
  * PadLock detection routine
  */
@@ -68,7 +67,6 @@ int mbedtls_padlock_has_support( int feature )
 
     return( flags & feature );
 }
-#endif
 
 /*
  * PadLock AES-ECB block en(de)cryption
