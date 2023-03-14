8577c2624e6549c8e18e1848390324bfe2cabbce
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-03-14 10:41:11 -0700

    ci: better triggering async tests

--- ct.sh
+++ ct.sh
@@ -25,9 +25,10 @@ if [ $TRAVIS_RUST_VERSION = "stable" ] || [ $TRAVIS_RUST_VERSION = "beta" ] || [
         cargo test --features spin_threading --target $TARGET
         cargo test --features rust_threading --target $TARGET
         cargo test --features custom_time,custom_gmtime_r --target $TARGET
-        # without these, tests marked with tokio::test do not run, but report OK.
-        cargo test --features=std,threading,tokio,tokio/net,tokio/io-util,tokio/macros,tokio/rt --target $TARGET
-         # If zlib is installed, test the zlib feature
+        # test the async support
+        cargo test --test async_session --features=std,threading,tokio,tokio/net,tokio/io-util,tokio/macros,tokio/rt --target $TARGET
+
+        # If zlib is installed, test the zlib feature
         if [ -n "$ZLIB_INSTALLED" ]; then
             cargo test --features zlib --target $TARGET
         fi
