2fc953fe6cb960d9b4e1d80a4929e4fcb7216be5
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-03-13 22:39:49 -0700

    ci: update ci
    update ci to be similar to master version

--- ct.sh
+++ ct.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 cd "$(dirname "$0")"
 
@@ -9,30 +9,40 @@ if [ -z $TRAVIS_RUST_VERSION ]; then
     exit 1
 fi
 
+export CFLAGS_x86_64_fortanix_unknown_sgx="-isystem/usr/include/x86_64-linux-gnu -mlvi-hardening -mllvm -x86-experimental-lvi-inline-asm-hardening"
+export CC_x86_64_fortanix_unknown_sgx=clang-11
+
 if [ $TRAVIS_RUST_VERSION = "stable" ] || [ $TRAVIS_RUST_VERSION = "beta" ] || [ $TRAVIS_RUST_VERSION = "nightly" ]; then
     rustup default $TRAVIS_RUST_VERSION
-    # make sure that explicitly providing the default target works
-    cargo test --target x86_64-unknown-linux-gnu
-    cargo test --release
-    cargo test --features spin_threading
-    cargo test --features rust_threading
-    cargo test --features custom_time,custom_gmtime_r
-    cargo test --features zlib
-    cargo test --features pkcs12
-    cargo test --features pkcs12_rc2
-    cargo test --features force_aesni_support
-    cargo test --features dsa
-    # without these, tests marked with tokio::test do not run, but report OK.
-    cargo test --features=std,threading,tokio,tokio/net,tokio/io-util,tokio/macros,tokio/rt
+    rustup target add --toolchain $TRAVIS_RUST_VERSION $TARGET
+    # The SGX target cannot be run under test like a ELF binary
+    if [ "$TARGET" != "x86_64-fortanix-unknown-sgx" ]; then
+        # make sure that explicitly providing the default target works
+        cargo test --target $TARGET --release
+        cargo test --features pkcs12 --target $TARGET
+        cargo test --features pkcs12_rc2 --target $TARGET
+        cargo test --features dsa --target $TARGET
+        cargo test --features spin_threading --target $TARGET
+        cargo test --features rust_threading --target $TARGET
+        cargo test --features custom_time,custom_gmtime_r --target $TARGET
+        # without these, tests marked with tokio::test do not run, but report OK.
+        cargo test --features=std,threading,tokio,tokio/net,tokio/io-util,tokio/macros,tokio/rt --target $TARGET
+         # If zlib is installed, test the zlib feature
+        if [ -n "$ZLIB_INSTALLED" ]; then
+            cargo test --features zlib --target $TARGET
+        fi
+
+        # If AES-NI is supported, test the feature
+        if [ -n "$AES_NI_SUPPORT" ]; then
+            cargo test --features force_aesni_support --target $TARGET
+        fi
+    else
+        cargo +$TRAVIS_RUST_VERSION test --no-run --target=$TARGET --features=sgx --no-default-features
+    fi
 
 elif [ $TRAVIS_RUST_VERSION = $CORE_IO_NIGHTLY ]; then
     cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand,time,custom_time,custom_gmtime_r
     cargo +$CORE_IO_NIGHTLY test --no-default-features --features core_io,rdrand
-
-elif [ $TRAVIS_RUST_VERSION = $SGX_NIGHTLY ]; then
-    rustup target add --toolchain $SGX_NIGHTLY x86_64-fortanix-unknown-sgx
-    cargo +$SGX_NIGHTLY test --no-run --target=x86_64-fortanix-unknown-sgx --features=sgx --no-default-features
-
 else
     echo "Unknown version $TRAVIS_RUST_VERSION"
     exit 1
