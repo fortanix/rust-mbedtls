0b5c0d9ae0515e3c7dd524530f6f340ae7f16790
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-04-19 15:02:51 -0700

    fix link and compile error

---
+++ mbedtls/build.rs
@@ -0,0 +1,14 @@
+/* Copyright (c) Fortanix, Inc.
+ *
+ * Licensed under the GNU General Public License, version 2 <LICENSE-GPL or
+ * https://www.gnu.org/licenses/gpl-2.0.html> or the Apache License, Version
+ * 2.0 <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0>, at your
+ * option. This file may not be copied, modified, or distributed except
+ * according to those terms. */
+
+fn main() {
+    // ensure able to link `custom_gmtime_r` correctly
+    println!("cargo:rustc-link-lib=static=mbedtls");
+    println!("cargo:rustc-link-lib=static=mbedx509");
+    println!("cargo:rustc-link-lib=static=mbedcrypto");
+}
