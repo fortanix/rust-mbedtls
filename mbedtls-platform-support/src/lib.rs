ad386cb986fe956d93a4c37a615e79a9307f06a4
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-05-15 10:33:56 -0700

    refactor(mbedtls-platform-support): update

--- mbedtls-platform-support/src/lib.rs
+++ mbedtls-platform-support/src/lib.rs
@@ -106,3 +106,95 @@ pub unsafe extern "C" fn mbedtls_time(tp: *mut mbedtls_sys::types::time_t) -> mb
     }
     timestamp
 }
+
+#[cfg(target_env = "sgx")]
+#[doc(hidden)]
+#[no_mangle]
+// needs to be pub for global visibility
+pub unsafe extern "C" fn explicit_bzero(buf: *mut mbedtls_sys::types::raw_types::c_void, len: mbedtls_sys::types::size_t)  {
+    use zeroize::Zeroize;
+    let buffer = core::slice::from_raw_parts_mut(buf as *mut mbedtls_sys::types::raw_types::c_char, len);
+    buffer.zeroize();
+}
+
+/// You need to call `psa_crypto_init()` before calling any function from the SSL/TLS, X.509 or PK modules.
+/// This function is fine to be called mutiple times while ensure underlying initilization function is only
+/// been called only once.
+/// Although this function is documented to be safely called multiple times, it still throws error in muti-thread case.
+/// Upstream document: https://arm-software.github.io/psa-api/crypto/1.1/api/library/library.html#c.psa_crypto_init
+/// See tracking issue: https://github.com/fortanix/rust-mbedtls/issues/285
+#[cfg(feature = "tls13")]
+pub fn psa_crypto_init() {
+    use once_cell::sync::OnceCell;
+    static INIT: OnceCell<()> = OnceCell::new();
+
+    INIT.get_or_init(|| {
+        unsafe { mbedtls_sys::psa::crypto_init() };
+        return ();
+    });
+}
+
+/// An implementation of an external random generator need to provided for Mbed TLS's PSA module when there is no RNG provided by platform, e.g. libc.
+/// This feature replaces the RNG calls in Mbed TLS's PSA module when enabled.
+///
+/// # Safety
+/// This function is marked `unsafe` as it's a function exposed to C to use.
+///
+///
+/// # Return
+/// This function returns zero on success or ::mbedtls_sys::ERR_ENTROPY_SOURCE_FAILED if it cannot generate random data.
+///
+/// # Notes
+/// * This function uses the hardware `RDRAND` instruction, if available (x86 only), as a source of randomness.
+#[cfg(sys_tls13_component = "external_entropy")]
+#[no_mangle]
+pub unsafe extern "C" fn mbedtls_psa_external_get_random(
+    _user_data: *mut mbedtls_sys::types::raw_types::c_void,
+    data: *mut mbedtls_sys::types::raw_types::c_uchar,
+    len: mbedtls_sys::types::size_t,
+    olen: *mut mbedtls_sys::types::size_t,
+) -> mbedtls_sys::types::int32_t {
+    // Intel documentation claims that if hardware is working RDRAND will produce
+    // output after at most 10 attempts
+    const RDRAND_READ_ATTEMPTS: usize = 10;
+
+    #[cfg(target_arch = "x86")]
+    use core::arch::x86_64::_rdrand32_step as rdrand_step;
+    #[cfg(target_arch = "x86_64")]
+    use core::arch::x86_64::_rdrand64_step as rdrand_step;
+
+    fn call_cpu_rng<T, F>(attempts: usize, intrin: unsafe fn(&mut T) -> i32, cast: F) -> Option<usize>
+    where
+        T: Sized + Default,
+        F: FnOnce(T) -> usize,
+    {
+        assert_eq!(core::mem::size_of::<T>(), core::mem::size_of::<usize>());
+
+        for _ in 0..attempts {
+            let mut out = T::default();
+            let status = unsafe { intrin(&mut out) };
+            if status == 1 {
+                return Some(cast(out));
+            }
+        }
+        None
+    }
+
+    // outbuf data/len are stack variables
+    let mut outbuf = core::slice::from_raw_parts_mut(data, len);
+
+    // rdrand function is thread safe
+    let stepsize = core::mem::size_of::<usize>();
+
+    for chunk in (&mut outbuf).chunks_mut(stepsize) {
+        if let Some(val) = call_cpu_rng(RDRAND_READ_ATTEMPTS, rdrand_step, |x| x as usize) {
+            let buf = val.to_ne_bytes();
+            let ptr = &buf[..chunk.len()];
+            chunk.copy_from_slice(ptr);
+        } else {
+            return ::mbedtls_sys::ERR_ENTROPY_SOURCE_FAILED;
+        }
+    }
+    *olen = len;
+    0
+}
