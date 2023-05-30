5f0f8f7fc05dc6bf42ba093febc9a9f1193344db
Tobias Naumann <tobias.naumann@iml.fraunhofer.de>
2023-05-30 19:28:08 +0200

    Allow name-mangling for the mbedtls_free, mbedtls_calloc and mbedtls_ssl_flush_output functions
    According to the discussion in
    https://github.com/rust-lang/rust-bindgen/issues/1221#issuecomment-442950753,
    the \u{1} is a hint to LLVM to avoid name-mangling but this might be required
    on some platforms to link against the right name, e.g. on macOS.

--- mbedtls/src/alloc.rs
+++ mbedtls/src/alloc.rs
@@ -15,9 +15,9 @@ use core::mem::ManuallyDrop;
 use mbedtls_sys::types::raw_types::c_void;
 
 extern "C" {
-    #[link_name = concat!("\u{1}forward_mbedtls_free_", env!("RUST_MBEDTLS_METADATA_HASH"))]
+    #[link_name = concat!("forward_mbedtls_free_", env!("RUST_MBEDTLS_METADATA_HASH"))]
     pub(crate) fn mbedtls_free(n: *mut mbedtls_sys::types::raw_types::c_void);
-    #[link_name = concat!("\u{1}forward_mbedtls_calloc_", env!("RUST_MBEDTLS_METADATA_HASH"))]
+    #[link_name = concat!("forward_mbedtls_calloc_", env!("RUST_MBEDTLS_METADATA_HASH"))]
     pub(crate) fn mbedtls_calloc(n: mbedtls_sys::types::size_t, size: mbedtls_sys::types::size_t) -> *mut mbedtls_sys::types::raw_types::c_void;
 }
 
