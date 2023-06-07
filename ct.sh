e82c1489bdb2713ed0c5894711557178f7725abc
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-06-06 18:17:08 -0700

    ci: update tested features

--- ct.sh
+++ ct.sh
@@ -61,7 +61,7 @@ if [ "$TRAVIS_RUST_VERSION" == "stable" ] || [ "$TRAVIS_RUST_VERSION" == "beta" 
         
         # If AES-NI is supported, test the feature
         if [ -n "$AES_NI_SUPPORT" ]; then
-            cargo nextest run --features force_aesni_support --target $TARGET
+            cargo nextest run --features force_aesni_support,tls13 --target $TARGET
         fi
 
         # no_std tests only are able to run on x86 platform
@@ -71,6 +71,7 @@ if [ "$TRAVIS_RUST_VERSION" == "stable" ] || [ "$TRAVIS_RUST_VERSION" == "beta" 
         fi
     else
         cargo +$TRAVIS_RUST_VERSION test --no-run --target=$TARGET
+        cargo +$TRAVIS_RUST_VERSION test --no-default-features --features dsa,force_aesni_support,mpi_force_c_code,rdrand,std,time,tls13 --no-run --target=$TARGET
     fi
 
 else
