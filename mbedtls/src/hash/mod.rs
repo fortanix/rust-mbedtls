d48bb83ee7472431564a2df518e1f4949e9266d1
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-06-26 16:22:22 -0700

    fix: stop using deprecated pkcs5_pbkdf2_hmac

--- mbedtls/src/hash/mod.rs
+++ mbedtls/src/hash/mod.rs
@@ -230,16 +230,9 @@ pub fn pbkdf2_hmac(
     iterations: u32,
     key: &mut [u8],
 ) -> Result<()> {
-    let md: MdInfo = match md.into() {
-        Some(md) => md,
-        None => return Err(codes::MdBadInputData.into()),
-    };
-
     unsafe {
-        let mut ctx = Md::init();
-        md_setup((&mut ctx).into(), md.into(), 1).into_result()?;
-        pkcs5_pbkdf2_hmac(
-            (&mut ctx).into(),
+        pkcs5_pbkdf2_hmac_ext(
+            md.into(),
             password.as_ptr(),
             password.len(),
             salt.as_ptr(),
