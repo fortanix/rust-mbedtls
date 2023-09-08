2dd834bb2e10d9bdfb8ed88c2459aed7ca7e039c
Yuxiang Cao <yuxiang.cao@fortanix.com>
2023-09-08 14:57:05 -0700

    ci: update CI for x86_64-pc-windows-msvc

--- ct.sh
+++ ct.sh
@@ -7,7 +7,9 @@ if [ -z $TRAVIS_RUST_VERSION ]; then
     exit 1
 fi
 
-# checks if a file has a specific sha512 hash, compatible for Linux and macOS
+# Setup dependencies and tools
+
+# checks if a file has a specific sha512 hash
 check_sha512() {
     local hash="$1"
     local file="$2"
@@ -18,6 +20,9 @@ check_sha512() {
             ;;
         Darwin)
             shasum -a 512 -c <<< "$hash *$file"
+            ;;
+        MINGW64_NT-*)
+            sha512sum -c <<< "$hash *$file"
             ;;
         *)
             echo "Unsupported platform '$platfom'"
@@ -37,15 +42,9 @@ if [ "$TARGET" == "aarch64-unknown-linux-musl" ]; then
     tar -xf ${aarch64_cross_toolchain_save_path} -C /tmp;
 fi
 
-export CFLAGS_x86_64_fortanix_unknown_sgx="-isystem/usr/include/x86_64-linux-gnu -mlvi-hardening -mllvm -x86-experimental-lvi-inline-asm-hardening"
-export CC_x86_64_fortanix_unknown_sgx=clang-11
-export CC_aarch64_unknown_linux_musl=/tmp/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=/tmp/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUNNER=qemu-aarch64
-
-# Setup dependencies and tools
-
 # According to `mbedtls-sys/vendor/README.md`, need to install needed pkgs
+python3 -m venv venv
+source venv/bin/activate || source venv/Scripts/activate
 python3 -m pip install -r ./mbedtls-sys/vendor/scripts/basic.requirements.txt
 
 # function for downloading pre-built `cargo-nextest` on various platforms
@@ -55,7 +54,14 @@ download_cargo_nextest() {
     local url="$3"
     echo "Check if need to download pre-built $platform 'cargo-nextest'"
     if ! check_sha512 "${cargo_nextest_hash}" "${CARGO_HOME:-$HOME/.cargo}/bin/cargo-nextest"; then
-        curl -LsSf "$url" | tar zxf - -C "${CARGO_HOME:-$HOME/.cargo}/bin"
+        case $platform in
+            MINGW64-*)
+                curl -LsSf "$url" -o temp.zip && unzip -d "${CARGO_HOME:-$HOME/.cargo}/bin" temp.zip && rm temp.zip
+                ;;
+            *)
+                curl -LsSf "$url" | tar zxf - -C "${CARGO_HOME:-$HOME/.cargo}/bin"
+                ;;
+        esac
         check_sha512 "${cargo_nextest_hash}" "${CARGO_HOME:-$HOME/.cargo}/bin/cargo-nextest"
     fi
 }
@@ -72,47 +78,55 @@ case "$kernel-$architecture" in
   Darwin-x86_64)
     download_cargo_nextest "Darwin-amd64" "0bb8b77ce019de3d06ee6b7382d830ed67309f187781e0de3866a0635879b494c7db48d55eee7553cfaa0bfca59abd8f8540a6d81ed703f06f9c81514d20073d" "https://get.nexte.st/0.9.52/mac"
     ;;
+  MINGW64_NT-*-x86_64)
+    download_cargo_nextest "MINGW64-amd64" "3ffd504a4ef0b4b5e988457e6c525e62bd030d46b8f303f1c4e83a9a8ba89aef34bb239e23f391d1dddb75bea6ff074499153b2c71b06338a05d74916408de9c" "https://get.nexte.st/0.9.52/windows"
+    ;;
   *)
     echo "Unknown platform '$kernel-$architecture'"
     exit 1
     ;;
 esac
 
-
 # Test logic start from here
+export CFLAGS_x86_64_fortanix_unknown_sgx="-isystem/usr/include/x86_64-linux-gnu -mlvi-hardening -mllvm -x86-experimental-lvi-inline-asm-hardening"
+export CC_x86_64_fortanix_unknown_sgx=clang-11
+export CC_aarch64_unknown_linux_musl=/tmp/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=/tmp/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUNNER=qemu-aarch64
+
 cd "./mbedtls"
-if [ "$TRAVIS_RUST_VERSION" == "stable" ] || [ "$TRAVIS_RUST_VERSION" == "beta" ] || [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
-    # Install the rust toolchain
-    rustup default $TRAVIS_RUST_VERSION
-    rustup target add --toolchain $TRAVIS_RUST_VERSION $TARGET
-    printenv
+case "$TRAVIS_RUST_VERSION" in
+    stable|beta|nightly)
+        # Install the rust toolchain
+        rustup default $TRAVIS_RUST_VERSION
+        rustup target add --toolchain $TRAVIS_RUST_VERSION $TARGET
+        printenv
 
-    # The SGX target cannot be run under test like a ELF binary
-    if [ "$TARGET" != "x86_64-fortanix-unknown-sgx" ]; then
-        # make sure that explicitly providing the default target works
-        cargo nextest run --target $TARGET --release
-        cargo nextest run --features dsa --target $TARGET
-        cargo nextest run --features async-rt,tls13 --target $TARGET
+        # The SGX target cannot be run under test like a ELF binary
+        if [ "$TARGET" != "x86_64-fortanix-unknown-sgx" ]; then
+            # make sure that explicitly providing the default target works
+            cargo nextest run --target $TARGET --release
+            cargo nextest run --features dsa --target $TARGET
+            cargo nextest run --features async-rt,tls13 --target $TARGET
 
-        # If AES-NI is supported, test the feature
-        if [ -n "$AES_NI_SUPPORT" ]; then
-            cargo nextest run --features force_aesni_support,tls13 --target $TARGET
+            # If AES-NI is supported, test the feature
+            if [ -n "$AES_NI_SUPPORT" ]; then
+                cargo nextest run --features force_aesni_support,tls13 --target $TARGET
+            fi
+            # no_std tests only are able to run on x86 platform
+            if [ "$TARGET" == "x86_64-unknown-linux-gnu" ] || [ "$TARGET" == "x86_64-apple-darwin" ] || [[ "$TARGET" =~ ^x86_64-pc-windows- ]]; then
+                cargo nextest run --no-default-features --features no_std_deps,rdrand,time --target $TARGET
+                cargo nextest run --no-default-features --features no_std_deps --target $TARGET
+            fi
+
+        else
+            cargo +$TRAVIS_RUST_VERSION test --no-run --target=$TARGET
+            cargo +$TRAVIS_RUST_VERSION test --no-default-features --features dsa,force_aesni_support,mpi_force_c_code,rdrand,std,time,tls13 --no-run --target=$TARGET
         fi
-
-        # no_std tests only are able to run on x86 platform
-        if [ "$TARGET" == "x86_64-unknown-linux-gnu" ]; then
-            cargo nextest run --no-default-features --features no_std_deps,rdrand,time --target $TARGET
-            cargo nextest run --no-default-features --features no_std_deps,rdrand --target $TARGET
-        fi
-        if [ "$TARGET" == "x86_64-apple-darwin" ]; then
-            cargo nextest run --no-default-features --features no_std_deps --target $TARGET
-        fi
-    else
-        cargo +$TRAVIS_RUST_VERSION test --no-run --target=$TARGET
-        cargo +$TRAVIS_RUST_VERSION test --no-default-features --features dsa,force_aesni_support,mpi_force_c_code,rdrand,std,time,tls13 --no-run --target=$TARGET
-    fi
-
-else
-    echo "Unknown version $TRAVIS_RUST_VERSION"
-    exit 1
-fi
+        ;;
+    *)
+        # Default case: If TRAVIS_RUST_VERSION does not match any of the above
+        echo "Unknown version $TRAVIS_RUST_VERSION"
+        exit 1
+        ;;
+esac
