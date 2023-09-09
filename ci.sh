e12b26121ff556813fdb1d2ee5843b4d39210b13
YxC <yuxiang.cao@fortanix.com>
2023-09-09 14:47:02 -0700

    ci: fix wrong path

--- ci.sh
+++ ci.sh
@@ -15,7 +15,7 @@ python3 -m pip install -r ./mbedtls-sys/vendor/scripts/basic.requirements.txt
 export CFLAGS_x86_64_fortanix_unknown_sgx="-isystem/usr/include/x86_64-linux-gnu -mlvi-hardening -mllvm -x86-experimental-lvi-inline-asm-hardening"
 export CC_x86_64_fortanix_unknown_sgx=clang-11
 export CC_aarch64_unknown_linux_musl=/tmp/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=/tmp/target/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=/tmp/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
 export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUNNER=qemu-aarch64
 
 cd "${repo_root}/mbedtls"
