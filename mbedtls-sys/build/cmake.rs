befa19bcbdbd760cd00c71469ae20708f53e2246
Yuxiang Cao <yuxiang.cao@fortanix.com>
2024-02-21 13:48:28 -0800

    resolve new warnings in nightly

--- mbedtls-sys/build/cmake.rs
+++ mbedtls-sys/build/cmake.rs
@@ -6,8 +6,6 @@
  * option. This file may not be copied, modified, or distributed except
  * according to those terms. */
 
-use cmake;
-
 impl super::BuildConfig {
     pub fn cmake(&self) {
         let mut cmk = cmake::Config::new(&self.mbedtls_src);
