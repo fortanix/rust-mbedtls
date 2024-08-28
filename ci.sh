2aba6567d0cc1363150dde10a8c2aee19145420c
Yuxiang Cao <yuxiang.cao@fortanix.com>
2024-08-28 14:56:14 -0700

    update and fix CI

--- ci.sh
+++ ci.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 
 repo_root=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 
-if [ -z $TRAVIS_RUST_VERSION ]; then
-    echo "Expected TRAVIS_RUST_VERSION to be set in env"
+if [ -z $RUST_VERSION ]; then
+    echo "Expected RUST_VERSION to be set in env"
     exit 1
 fi
 
@@ -17,11 +17,11 @@ export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=/tmp/aarch64-linux-musl-cr
 export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUNNER=qemu-aarch64
 
 cd "${repo_root}/mbedtls"
-case "$TRAVIS_RUST_VERSION" in
+case "$RUST_VERSION" in
     stable|beta|nightly)
         # Install the rust toolchain
-        rustup default $TRAVIS_RUST_VERSION
-        rustup target add --toolchain $TRAVIS_RUST_VERSION $TARGET
+        rustup default $RUST_VERSION
+        rustup target add --toolchain $RUST_VERSION $TARGET
         printenv
 
         for FEAT in "" "x509," "ssl,"; do
@@ -44,7 +44,7 @@ case "$TRAVIS_RUST_VERSION" in
                     cargo nextest run --no-default-features --features "$FEAT"no_std_deps --target $TARGET
                 fi
             else
-                cargo +$TRAVIS_RUST_VERSION test --no-run --features "$FEAT" --target=$TARGET
+                cargo +$RUST_VERSION test --no-run --features "$FEAT" --target=$TARGET
             fi
         done
 
@@ -71,8 +71,8 @@ case "$TRAVIS_RUST_VERSION" in
         fi
         ;;
     *)
-        # Default case: If TRAVIS_RUST_VERSION does not match any of the above
-        echo "Unknown version $TRAVIS_RUST_VERSION"
+        # Default case: If RUST_VERSION does not match any of the above
+        echo "Unknown version $RUST_VERSION"
         exit 1
         ;;
 esac
