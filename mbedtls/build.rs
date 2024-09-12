bb51a6a03ca022e7b79b8486c8f7bbc2463c5a85
Iraj <iraj.taghlidi@fortanix.com>
2024-09-12 10:31:44 +0200

    Improve OUT_DIR path processing in Bazel part
    C

--- mbedtls/build.rs
+++ mbedtls/build.rs
@@ -22,21 +22,22 @@ fn get_compilation_symbol_suffix() -> String {
     let mut out_dir_it_rev = out_dir.iter().rev();
     if out_dir_it_rev.next().map_or(false, |p| p == "out") {
         // If Cargo is used as build system.
-        let crate_ = out_dir_it_rev.next().unwrap().to_string_lossy();
-        assert!(crate_.starts_with("mbedtls-"), "Expected directory to start with 'mbedtls-'");
+        let crate_ = out_dir_it_rev.next()
+            .expect("Expected OUT_DIR to have at least 2 components")
+            .to_str()
+            .expect("Expected second to last component of OUT_DIR to be a valid UTF-8 string");
+        assert!(crate_.starts_with("mbedtls-"), "Expected second to last component of OUT_DIR to start with 'mbedtls-'");
         return crate_[8..].to_owned(); // Return the part after "mbedtls-"
-    } else if out_dir.iter().rfind(|p| p.to_string_lossy() == "bazel-out").is_some() {
+    } else if out_dir.iter().rfind(|p| *p == "bazel-out").is_some() {
+        // If Bazel is used as build system.
         let mut hasher = DefaultHasher::new();
-        // Reverse the iterator and hash until you find "bazel-out"
-        for p in out_dir.iter().rev().take_while(|p| p.to_string_lossy() != "bazel-out") {
+        // Reverse the iterator and hash until we find "bazel-out"
+        for p in out_dir.iter().rev().take_while(|p| *p != "bazel-out") {
             p.hash(&mut hasher);
         }
-        
-        let hash = hasher.finish();
-        return format!("{:016x}", hash);
-    }
-     else {
-        panic!("unexpected OUT_DIR format: {}", out_dir.to_string_lossy());
+        return format!("{:016x}", hasher.finish());
+    } else {
+        panic!("unexpected OUT_DIR format: {}", out_dir.display());
     }
 }
 
