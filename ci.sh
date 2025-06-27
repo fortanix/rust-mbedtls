02c16163647161a9906027706df5e4b95caea48e
Yuxiang Cao <yuxiang.cao@fortanix.com>
2025-06-26 23:32:25 -0700

    simplify tests

--- ci.sh
+++ ci.sh
@@ -18,7 +18,6 @@ cargo_nextest="cargo nextest run"
 cargo_test="cargo test"
 
 if [ "$TARGET" == "aarch64-unknown-linux-musl" ]; then
-    cargo install cross --git https://github.com/cross-rs/cross
     cargo_nextest="cross test"
     cargo_test="cross test"
 fi
@@ -35,13 +34,9 @@ case "$RUST_VERSION" in
         if [ "$TARGET" != "x86_64-fortanix-unknown-sgx" ]; then
             # make sure that explicitly providing the default target works
             $cargo_nextest --target $TARGET --release
-            $cargo_nextest --features pkcs12 --target $TARGET
-            $cargo_nextest --features pkcs12_rc2 --target $TARGET
-            $cargo_nextest --features dsa --target $TARGET
-            $cargo_nextest --features x509 --target $TARGET
-            $cargo_nextest --features ssl --target $TARGET
+            $cargo_nextest --features pkcs12_rc2,dsa,ssl --target $TARGET
 
-                # If AES-NI is supported, test the feature
+            # If AES-NI is supported, test the feature
             if [ -n "$AES_NI_SUPPORT" ]; then
                 $cargo_nextest --features force_aesni_support --target $TARGET
                 $cargo_nextest --features force_aesni_support,x509,ssl --target $TARGET
@@ -50,13 +45,11 @@ case "$RUST_VERSION" in
             # no_std tests only are able to run on x86 platform
             if [ "$TARGET" == "x86_64-unknown-linux-gnu" ] || [[ "$TARGET" =~ ^x86_64-pc-windows- ]]; then
                 $cargo_nextest --no-default-features --features no_std_deps --target $TARGET
-                $cargo_nextest --no-default-features --features no_std_deps,rdrand,time,x509 --target $TARGET
                 $cargo_nextest --no-default-features --features no_std_deps,rdrand,time,ssl --target $TARGET
             fi
         else
             $cargo_test --no-run --target=$TARGET
             $cargo_test --no-run --features ssl --target=$TARGET
-            $cargo_test --no-run --features x509 --target=$TARGET
         fi
 
         if [ "$TARGET" == "x86_64-apple-darwin" ]; then
@@ -69,14 +62,12 @@ case "$RUST_VERSION" in
 
         # The SGX target cannot be run under test like a ELF binary
         if [ "$TARGET" != "x86_64-fortanix-unknown-sgx" ]; then
-            $cargo_nextest --test async_session --features=async-rt,ssl --target $TARGET
             $cargo_nextest --test async_session --features=async-rt,ssl,legacy_protocols --target $TARGET
             $cargo_nextest chrono --features=chrono,ssl,x509 --target $TARGET
 
             # If zlib is installed, test the zlib feature
             if [ -n "$ZLIB_INSTALLED" ]; then
                 $cargo_nextest --features zlib --target $TARGET
-                $cargo_nextest --test async_session --features=async-rt,ssl,zlib --target $TARGET
                 $cargo_nextest --test async_session --features=async-rt,ssl,zlib,legacy_protocols --target $TARGET
             fi
         fi
